152cbbecab96d845b146c01de2e97a3dfdcae7c8
ubaidi10 <17-ECT-10@students.uettaxila.edu.pk>
2024-11-22 09:19:49 +0500

    Analysis Script Update

--- scripts/analysis.sql
+++ scripts/analysis.sql
@@ -1,25 +1,4 @@
-/* SELECT Date,Google,Baidu FROM 'raw_data/search_engine_data.csv'
-ORDER BY Date DESC
-LIMIT 3; */
-SELECT Header, Value
-FROM (
-    SELECT 'Date' AS Header, Date AS Value
-    FROM 'raw_data/search_engine_data.csv'
-    WHERE Date = '2009-01'
-
-    UNION ALL
-
-    SELECT Header, Value
-    FROM (
-        SELECT *
-        FROM 'path/to/search_engine_data.csv'
-        WHERE Date = '2009-01'
-    ) UNPIVOT (Value FOR Header IN (
-        Google, bing, "Yahoo!", Baidu, YANDEX, "YANDEX RU",
-        "Ask Jeeves", DuckDuckGo, Naver, AOL, Haosou, Sogou, Babylon,
-        Shenma, Seznam, Conduit, MSN, "Mail.ru", Ecosia, Webcrawler,
-        Daum, CocCoc, "StartPagina (Google)", "AVG Search", SweetIM,
-        "Windows Live", Other
-    ))
-)
-ORDER BY Value DESC;
+ SELECT Date,Google,Baidu FROM 'raw_data/search_engine_data.csv'
+WHERE Date = '08-2024'
+ORDER BY Google DESC
+LIMIT 3;
